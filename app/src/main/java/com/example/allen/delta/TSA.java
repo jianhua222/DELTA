70a5ec0b2c5cd0273be7280db39f11035b1e9e9b
jianhua222 <1993222king@gmail.com>
2016-09-24 16:43:16 -0400

    TSA api creaated

---
+++ app/src/main/java/com/example/allen/delta/TSA.java
@@ -0,0 +1,25 @@
+package com.example.allen.delta;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * Created by Allen on 9/24/2016.
+ */
+
+public interface TSA {
+    @GET("hack/search/airport")
+    Call<Flight_States_Class> getFlight(
+            @Query("airport") String Airport,
+
+            @Query("apikey") String apiKey
+    );
+
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://demo30-test.apigee.net/v1/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+}
---
+++ app/src/main/java/com/example/allen/delta/TSA_Class.java
@@ -0,0 +1,14 @@
+package com.example.allen.delta;
+import java.util.List;
+
+/**
+ * Created by Allen on 9/24/2016.
+ */
+
+public class TSA_Class {
+    private WaitTimeResult waitTimeResult;
+    public class WaitTimeResult{
+        private List<String> waitTime;
+
+    }
+}
