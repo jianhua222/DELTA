205880cc6ea08f04e26ec2220017fced42e2be94
jianhua222 <1993222king@gmail.com>
2016-09-24 21:13:20 -0400

    TSA api creaated

--- app/src/main/java/com/example/allen/delta/MainActivity.java
+++ app/src/main/java/com/example/allen/delta/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.allen.delta;
 
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 
 import java.util.List;
+import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -16,72 +20,85 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    String dateSt="", flightSt="";
+    String dateSt = "", flightSt = "";
     String departureCodeSt = "";
-    int averageWaitTime = 0;
+    //public static ArrayList<Integer> sum = new ArrayList<>();
+    int sum = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //tryButtonClick();
 
 
     }
 
-    public void setDepartureCode(String inputSt){
+    public void setDepartureCode(String inputSt) {
         departureCodeSt = inputSt;
 
         TextView textView = (TextView) findViewById(R.id.textView);
         textView.setText(departureCodeSt);
     }
 
-    public int getTimeFromWaitTime(String inputSt){
+    public int getTimeFromWaitTime(String inputSt) {
         int res = 0;
         String st = "";
         int index = inputSt.indexOf('+');
         if (index == -1)
             index = inputSt.indexOf(' ');
         //Log.v("allam", ""+index);
-        for (int i = index-1; i>=0; i--){
-            if (inputSt.charAt(i) >= '0' && inputSt.charAt(i) <= '9'){
+        for (int i = index - 1; i >= 0; i--) {
+            if (inputSt.charAt(i) >= '0' && inputSt.charAt(i) <= '9') {
                 st = inputSt.charAt(i) + st;
                 //Log.v("allam", st);
-            }
-            else break;
+            } else break;
         }
         //Log.v("allam", st);
         res = Integer.parseInt(st);
         return res;
     }
-    public void getTSATime(){
+
+    public void getTSATime() {
         TSA service = TSA.retrofit.create(TSA.class);
 
         Call<TSA_Class> call = service.getFlight(departureCodeSt, getString(R.string.delta_api_key));
 
         Log.v("allam", "Call url: " + call.request().url());
-        call.enqueue(new Callback<TSA_Class>(){
+        call.enqueue(new Callback<TSA_Class>() {
             @Override
-            public void onResponse(Call<TSA_Class> call, Response<TSA_Class> response){
+            public void onResponse(Call<TSA_Class> call, Response<TSA_Class> response) {
 
                 TSA_Class baseObject = response.body();
                 List<TSA_Class.waitTime> list = baseObject.getWaitTime();
 
-                for (int i = 0; i < list.size(); i++){
+                for (int i = 0; i < list.size(); i++) {
 
 
-                    Log.v("allam", list.get(i).getWaittime());
-                    int t = getTimeFromWaitTime((list.get(i).getWaittime()));
-                    Log.v("allam", "" + t);
+                    //Log.v("allam", list.get(i).getWaittime());
+                    Integer t = getTimeFromWaitTime((list.get(i).getWaittime()));
+                    sum += t;
+                    //sum.add(t);
+                    //Log.v("allam", "" + t);
                 }
+                sum = sum / list.size();
+                Log.v("allam", ""+sum);
 
+                Intent resultIntent = new Intent(MainActivity.this, ResultActivity.class);
+                resultIntent.putExtra("airportCode", departureCodeSt);
+                resultIntent.putExtra(ResultActivity.WAIT_AVERAGE, sum);
+                Log.v("allam", ""+sum);
+                startActivity(resultIntent);
             }
+
             @Override
-            public void onFailure(Call<TSA_Class> call, Throwable t){
+            public void onFailure(Call<TSA_Class> call, Throwable t) {
                 Log.v("allam", t.getMessage());
             }
         });
     }
-    public void getDepartureCode(){
+
+    public void getDepartureCode() {
         Flight_States service = Flight_States.retrofit.create(Flight_States.class);
 
         Call<Flight_States_Class> call = service.getFlight(flightSt, dateSt, getString(R.string.delta_api_key));
@@ -110,7 +127,7 @@ public class MainActivity extends AppCompatActivity {
         //return departureAirportCode;
     }
 
-    public void clickEvent(View v){
+    public void clickEvent(View v) {
         EditText fightNumberInput = (EditText) findViewById(R.id.flightNumberInput);
         EditText dateInput = (EditText) findViewById(R.id.dateInput);
         dateSt = dateInput.getText().toString();
@@ -120,4 +137,12 @@ public class MainActivity extends AppCompatActivity {
         getDepartureCode();
     }
 
+    public void tryButtonClick() {
+        Button click = (Button) findViewById(R.id.button);
+        click.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+    }
 }
---
+++ app/src/main/java/com/example/allen/delta/ResultActivity.java
@@ -0,0 +1,44 @@
+package com.example.allen.delta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class ResultActivity extends AppCompatActivity {
+    public static final String WAIT_AVERAGE = "waitAverage";
+    //ArrayList<Integer> input;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        //input =  MainActivity.sum;
+        //input = getIntent().getAc //((MainActivity)savedInstanceState).sum;
+        //getAverage(input);
+
+        int sum = getIntent().getIntExtra(WAIT_AVERAGE, 0);
+        String departureCode = getIntent().getStringExtra("airportCode");
+
+        Log.v("allam", "here?: "+sum);
+        TextView textView = (TextView) findViewById(R.id.textView2);
+        textView.setText(""+sum);
+    }
+//
+//    public void getAverage(ArrayList<Integer> input){
+//
+//        Log.v("allam", "" + input.size());
+//        int counter = 0;
+//        int sum = 0;
+//            sum =+ input.get(i);
+//            counter++;
+//        }
+//        for(int i = 0 ; i< input.size(); i++){
+//        TextView textView = (TextView) findViewById(R.id.textView2);
+//        textView.setText((sum/counter));
+//
+//    }
+}
